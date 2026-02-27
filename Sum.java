c34865aa4511ac7251ff305b41e68a3c2860c265
DharitrNayak <dharatrinayak147@gmail.com>
2026-02-27 23:47:30 +0530

    Added Sum Java program

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    int a=10; 
+    int b=25; 
+    int Sum=a+b;
+     public static void main(String[] args) 
+     {
+        Sum o= new Sum(); 
+        System.out.println(o.Sum); } 
+}
